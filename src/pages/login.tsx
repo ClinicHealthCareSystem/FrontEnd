6ee2d170196daa3c6686221a8874c4edbd98160c
Fernando <fernandocr019@gmail.com>
2025-08-12 19:09:27 -0300

    TSK-3 feat: faz conexao funcional com o backend de login do cliente

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,75 +1,99 @@
 import React from "react";
-import {useForm, Controller} from "react-hook-form";
+import { useForm, Controller } from "react-hook-form";
 import {
-  Text, TextInput, TouchableOpacity,
-  View, Image, ScrollView,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+  Image,
+  ScrollView,
 } from "react-native";
 import styles from "../styles/login";
 
-export default function Login(){
-  const {control, handleSubmit, formState: {errors}} = useForm({})
-  async function handleSignUp(data: any){
+export default function Login() {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({});
+  async function handleSignIp(data: any) {
     console.log(data);
-    try{
-      const response = await fetch(`http://localhost:3000/user/login`,{
-        method: "GET",
+    try {
+      const response = await fetch(`http://localhost:3000/user/signIn`, {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
-        body: JSON.stringify(data)
-    })
-    }
-    catch (error){
-      console.log("Não foi possivel criar o usuário" + error);
-
+        body: JSON.stringify(data),
+      });
+      console.log(response);
+      const json = await response.json();
+      console.log(response.status);
+      console.log(json);
+    } catch (error) {
+      console.log("Crendenciais incorretas, por favor tente novamente" + error);
     }
   }
   return (
     <ScrollView contentContainerStyle={styles.background}>
       <View style={styles.caixa}>
-        <View style={styles.caixaTitulo}> 
+        <View style={styles.caixaTitulo}>
+          <Image
+            source={require("../assests/heart-pulse (2).png")}
+            resizeMode="contain"
+            style={styles.logo}
+          ></Image>
 
-          <Image source={require("../assests/heart-pulse (2).png")} resizeMode="contain"
-          style = {styles.logo}></Image>
-          
-         <View>
-        <Text style={styles.titulo}>Saúde Mania </Text>
-        <Text style={styles.subTitulo}>Login</Text>
-          </View> 
-         </View>
-
+          <View>
+            <Text style={styles.titulo}>Saúde Mania </Text>
+            <Text style={styles.subTitulo}>Login</Text>
+          </View>
+        </View>
 
         <Text style={styles.label}>CPF</Text>
-        <Controller control={control} name="CPF" render={({field: {onChange, value}}) =>(
-          <TextInput style={styles.input} onChangeText={(text: string)=>onChange(text)} value={value} placeholder="Digite seu CPF"/>
-        )}/>
-        
+        <Controller
+          control={control}
+          name="CPF"
+          render={({ field: { onChange, value } }) => (
+            <TextInput
+              style={styles.input}
+              onChangeText={(text: string) => onChange(text)}
+              value={value}
+              placeholder="Digite seu CPF"
+            />
+          )}
+        />
 
         <Text style={styles.label}>Senha</Text>
-        <Controller control={control} name="password" render={({field: {onChange, value}}) =>(
-            <TextInput style={styles.input}  onChangeText={(text: string) => onChange(text)} value={value} placeholder="Digite sua Senha"/>
-        )}/>
-        
+        <Controller
+          control={control}
+          name="password"
+          render={({ field: { onChange, value } }) => (
+            <TextInput
+              style={styles.input}
+              onChangeText={(text: string) => onChange(text)}
+              value={value}
+              placeholder="Digite sua Senha"
+            />
+          )}
+        />
 
-        
-        <TouchableOpacity style={styles.buttonSenha}>  
+        <TouchableOpacity style={styles.buttonSenha}>
           <Text style={styles.textEsqueceu}>Esqueceu sua senha?</Text>
         </TouchableOpacity>
-        
 
-
-        <TouchableOpacity style={styles.button}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={handleSubmit(handleSignIp)}
+        >
           <Text style={styles.buttonText}>Entrar</Text>
         </TouchableOpacity>
 
         <Text style={styles.textinfo}>Você ainda possui uma conta?</Text>
-        <TouchableOpacity style={styles.buttonCadastro} onPress={handleSubmit(handleSignUp)}>  
-            <Text style={styles.buttonTextCadastro}>Cadastre-se</Text>
+        <TouchableOpacity style={styles.buttonCadastro}>
+          <Text style={styles.buttonTextCadastro}>Cadastre-se</Text>
         </TouchableOpacity>
-
       </View>
     </ScrollView>
   );
 }
-
-
