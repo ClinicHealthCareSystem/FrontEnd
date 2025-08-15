0542d44fcfccacd3e168c8538d5fdc6adef31101
Fernando <fernandocr019@gmail.com>
2025-08-15 12:10:58 -0300

    TSK-3 fix: devolve o "esqueceu sua senha"

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,5 +1,11 @@
 import React, { useState } from "react";
-import { Text, TextInput, TouchableOpacity, View, ScrollView } from "react-native";
+import {
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+  ScrollView,
+} from "react-native";
 import styles from "../styles/login";
 import { useRouter } from "expo-router";
 
@@ -9,18 +15,18 @@ export default function Login() {
   const [error, setError] = useState("");
   const router = useRouter();
 
-  const handleSignIn= async()=> {
+  const handleSignIn = async () => {
     setError("");
 
-    if (!CPF || !password){
+    if (!CPF || !password) {
       setError("Esses campos são obrigatórios");
       return;
     }
-    if (!/^\d{1,11}$/.test(CPF)){
+    if (!/^\d{1,11}$/.test(CPF)) {
       setError("CPF inválido somente números, máx. 11 dígitos");
       return;
     }
-    if (!/^[A-Z a-z 0-9]{1,6}$/.test(password)){
+    if (!/^[A-Z a-z 0-9]{1,6}$/.test(password)) {
       setError("Senha inválida máx. 6 caracteres, sem espaço ou especial");
       return;
     }
@@ -28,12 +34,12 @@ export default function Login() {
       const response = await fetch(`http://localhost:3000/user/signIn`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({CPF, password}),
+        body: JSON.stringify({ CPF, password }),
       });
       const json = await response.json();
       console.log(response.status, json);
-    } catch (err){
-      console.log("Erro ao logar: "+ err);
+    } catch (err) {
+      console.log("Erro ao logar: " + err);
     }
   };
 
@@ -47,7 +53,7 @@ export default function Login() {
           style={styles.input}
           placeholder="CPF"
           value={CPF}
-          onChangeText={text => setCPF(text.replace(/[^0-9]/g,""))}
+          onChangeText={(text) => setCPF(text.replace(/[^0-9]/g, ""))}
           keyboardType="numeric"
           maxLength={11}
         />
@@ -58,20 +64,28 @@ export default function Login() {
           style={styles.input}
           placeholder="Senha"
           value={password}
-          onChangeText={text => setPassword(text.replace(/[^A-Za-z0-9]/g,""))}
+          onChangeText={(text) =>
+            setPassword(text.replace(/[^A-Za-z0-9]/g, ""))
+          }
           secureTextEntry
           maxLength={6}
         />
       </View>
 
-      {error ? <Text style={{ color:"red", marginBottom:10 }}>{error}</Text>:null}
+      {error ? (
+        <Text style={{ color: "red", marginBottom: 10 }}>{error}</Text>
+      ) : null}
+
+      <TouchableOpacity>
+        <Text style={styles.esqueci}>Esqueceu a senha?</Text>{" "}
+      </TouchableOpacity>
 
       <TouchableOpacity style={styles.button} onPress={handleSignIn}>
         <Text style={styles.buttonText}>Acessar</Text>
       </TouchableOpacity>
       <View style={styles.cadastroCaixa}>
         <Text style={styles.text}>Não tem uma conta?</Text>
-        <TouchableOpacity onPress={()=>router.replace("/cadastro")}>
+        <TouchableOpacity onPress={() => router.replace("/cadastro")}>
           <Text style={styles.cadastro}>Cadastre-se</Text>
         </TouchableOpacity>
       </View>
