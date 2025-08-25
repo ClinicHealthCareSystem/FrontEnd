946ca63371038c0fc839a821e507fe8b0a5682d9
Lucas <lucasvieiraribeiro2055@gmail.com>
2025-08-25 09:13:49 -0300

    TSK13- fix ajusta HomePage

--- src/screens/Menu.tsx
+++ src/screens/Menu.tsx
@@ -24,75 +24,78 @@ export default function Menu() {
     {
       id: 2,
       title: "EXAMES",
-      descricao: "Marque sua consulta",
+      descricao: "Marque seu Enxame",
       icon: "flask-outline",
     },
     {
       id: 3,
       title: "HISTÓRICO",
-      descricao: "Marque sua consulta",
+      descricao: "Veja seus Histórico",
       icon: "time-outline",
     },
     {
       id: 4,
       title: "MEDICAMENTOS",
-      descricao: "Marque sua consulta",
+      descricao: "Qual Medicamento receitado",
       icon: "bandage-outline",
     },
     {
       id: 5,
       title: "RESULTADOS",
-      descricao: "Marque sua consulta",
-      icon: "people-outline",
+      descricao: "Veja os Resultados Clinicos",
+      icon: "document-text-outline",
     },
     {
       id: 6,
       title: "PLANOS",
-      descricao: "Marque sua consulta",
+      descricao: "Atualize seu plano",
       icon: "heart-circle-outline",
     },
   ];
   return (
-    <>
-      <View style={styles.background}>
-        <View style={styles.barraAlta}>
+    <View style={styles.background}>
+      <View style={styles.barraAlta}>
+        <View style={styles.caixaTopo}>
           <Text style={styles.tittle}>Saúde Mania</Text>
-          <TextInput style={styles.search_bar} placeholder="O que você está procurando?"></TextInput>
+          <TouchableOpacity>
+          <Ionicons name="notifications" size={20} color= "white"  style={{left: 150, bottom: 25}}/>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.caixaPesquisa}>
+        <Ionicons name="search" size={20} color= "#3284f1"  style={{top: 37}}/>
+        <TextInput style={styles.search_bar} placeholder="  O que você está procurando?"/>
         </View>
       </View>
 
       <ScrollView style={styles.scrollCards}>
         <View style={styles.cards}>
           {card.map((item) => (
-            <><TouchableOpacity key={item.id} style={styles.buttonCards}>
-              <Ionicons name={item.icon} size={36} color={"white"} />
+            <TouchableOpacity key={item.id} style={styles.buttonCards}>
+              <Ionicons name={item.icon} size={40} color={"white"} />
               <Text style={styles.textCards}>{item.title}</Text>
               <Text style={styles.descricaoCards}>{item.descricao}</Text>
-            </TouchableOpacity></>
+            </TouchableOpacity>
           ))}
-
-          
         </View>
-        
-      
-      
-      </ScrollView>
-      <View style={styles.teste}>
-            <TouchableOpacity style={styles.testeButton}>
-              <Ionicons name="home" size={30} color="#fff" />
-              <Text style={styles.textTeste}>Home</Text>
-              
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.testeButton}>
-              <Ionicons name="person" size={30} color="#fff" />
-              <Text style={styles.textTeste}>Perfil</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.testeButton}>
-              <Ionicons name="settings" size={30} color="#fff" />
-              <Text style={styles.textTeste}>Config</Text>
-            </TouchableOpacity>
 
-          </View>
-    </>
+      </ScrollView>
+
+      <View style={styles.tabNavegation}>
+        <TouchableOpacity style={styles.tabButton}>
+          <Ionicons name="home" size={30} color= "#fff" />
+          <Text style={styles.tabText}>Home</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.tabButton}>
+          <Ionicons name="person" size={30} color= "#fff" />
+          <Text style={styles.tabText}>Perfil</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.tabButton}>
+          <Ionicons name="settings" size={30} color= "#fff" />
+          <Text style={styles.tabText}>Config</Text>
+        </TouchableOpacity>
+        </View>
+    </View>
   );
 }
--- src/styles/menu.ts
+++ src/styles/menu.ts
@@ -1,4 +1,6 @@
 import { StyleSheet } from "react-native";
+import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
+
 
 export default StyleSheet.create({
   background: {
@@ -8,92 +10,109 @@ export default StyleSheet.create({
   },
 
   barraAlta: {
-    backgroundColor: "#3284f1",
-    height: 200,
-    borderBottomLeftRadius: 12,
-    borderBottomRightRadius: 12,
+    
+    backgroundColor: "#3284f1", 
+    paddingBottom: 25,
+    paddingHorizontal: 15,
+    borderBottomLeftRadius: 18,
+    borderBottomRightRadius: 18,
+    elevation: 6,    
+  },
+  caixaTopo:{
+    
+    justifyContent: "center",
     alignItems: "center",
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.8,
-    shadowRadius: 8,
+    marginTop: 25,
   },
   tittle: {
-    fontSize: 30,
-    marginTop: 20,
+    
+    fontSize: 28,
     fontWeight: "bold",
-    color: "#FFF",
-  },
+    color: "white"  
+},
+caixaPesquisa:{
+
+},
   search_bar:{
-    backgroundColor: "white",
-    width: 350,
+    
+     backgroundColor: "white",
     height: 40,
-    borderRadius: 8,
-    marginTop: 20,
-    paddingHorizontal: 10,
+    borderRadius: 12,
+    marginTop: 5,
+    paddingHorizontal: 12,
     fontSize: 16,
     color: "#797979ff",
+    outlineWidth: 0,
+
   },
   scrollCards: {
-    backgroundColor: "#FFF",
+   
+    marginTop: 10,
   },
   cards: {
-    backgroundColor: "",
-    flex: 1,
     flexDirection: "row",
     flexWrap: "wrap",
     justifyContent: "space-between",
-    padding: 10,
+    padding: 15,
     
   },
   buttonCards: {
+   
     backgroundColor: "#3284f1",
-    borderRadius: 10,
-    marginBottom: 15,
-    marginTop: 50,
-    padding: 15,
-    width: 100,
-    height: 100,
+    borderRadius: 8,
+    marginBottom: 16,
+    width: "47%",
+    height: 128,
     justifyContent: "center",
     alignItems: "center",
-    alignSelf: "center",
+    elevation: 5,
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.7,
     shadowRadius: 8,
-    borderWidth: 0,
+    borderWidth: 1,
     borderColor: "white",
+
+    
+
   },
   textCards: {
+   
     fontSize: 16,
     color: "white",
     fontWeight: "bold",
     textAlign: "center",
+    marginTop: 8,
   },
   descricaoCards: {
-    fontSize: 10,
+    
+    fontSize: RFValue(16),
     color: "white",
     textAlign: "center",
   },
-  teste:{
-    flexDirection: "row",
+  tabNavegation:{
+   
+     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
-    height: 60,
-    padding: 20,
+    height: 65,
     borderRadius: 2,
-    borderTopLeftRadius: 12,
-    borderTopRightRadius: 12,
+    borderTopLeftRadius: 18,
+    borderTopRightRadius: 18,
     backgroundColor: "#3284f1",
+    elevation:8,
+    borderTopWidth: 1,
+    borderColor: "white",
+    
 
   },
-  testeButton:{
+  tabButton:{
         flex: 1,
         alignItems: "center",
         color: "#FFF",
-        // flexDirection: "row",
-        // justifyContent: "space-between",
+        
   },
 
-  textTeste:{
+  tabText:{
     color: "#FFF",
 
     
