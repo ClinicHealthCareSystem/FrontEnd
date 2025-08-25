3f1da04b9c9df021d689df08f3f4ae7e2716792a
Fernando <fernandocr019@gmail.com>
2025-08-25 19:36:00 -0300

    TSK13 feat: Adiciona tela de agendar cadastro

---
+++ src/styles/consulta.ts
@@ -0,0 +1,178 @@
+import { StyleSheet } from "react-native";
+import { RFValue } from "react-native-responsive-fontsize";
+
+export default StyleSheet.create({
+  background: {
+    backgroundColor: "#ADD8E6",
+    flex: 1,
+  },
+
+  // header
+  headerView: {
+    backgroundColor: "#3284f1",
+    paddingBottom: 25,
+    borderBottomLeftRadius: 18,
+    borderBottomRightRadius: 18,
+  },
+
+  headerContent: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginTop: 25,
+  },
+
+  headerTitle: {
+    fontSize: RFValue(28),
+    fontWeight: "bold",
+    color: "white",
+  },
+
+  headerNotificationContainer: {
+    flex: 1,
+    alignItems: "flex-end",
+  },
+
+  headerNotifications: {
+    color: "white",
+    fontSize: RFValue(23),
+    right: 45,
+    top: 2,
+  },
+
+  headerInfo: {
+    alignItems: "center",
+    marginTop: 15,
+  },
+
+  headerInfoText: {
+    color: "white",
+    fontSize: RFValue(15),
+  },
+
+  // body
+  bodyView: {
+    marginTop: 15,
+    borderColor: "white",
+    width: "90%",
+    alignSelf: "center",
+  },
+
+  formLabel: {
+    fontSize: RFValue(16),
+    fontWeight: "bold",
+    color: "#0D47AB",
+    marginBottom: 6,
+  },
+
+  formInput: {
+    flexDirection: "row",
+    alignItems: "center",
+    width: "100%",
+    height: 55,
+    backgroundColor: "white",
+    borderRadius: 8,
+    marginBottom: 15,
+  },
+
+  formIcon: {
+    fontSize: RFValue(20),
+    marginRight: 5,
+    marginLeft: 5,
+  },
+
+  setDate: {
+    flex: 1,
+    padding: 12,
+    fontSize: 16,
+    borderWidth: 0,
+    backgroundColor: "#fff",
+  },
+
+  setTime: {
+    flex: 1,
+    padding: 12,
+    fontSize: 16,
+    borderRadius: 8,
+    borderWidth: 0,
+    backgroundColor: "#fff",
+  },
+
+  formPicker: {
+    borderWidth: 0,
+    backgroundColor: "transparent",
+    outlineWidth: 0,
+    marginRight: 10,
+  },
+
+  formTextInput: {
+    flex: 1,
+    height: "100%",
+    overflow: "hidden",
+    outlineWidth: 0,
+  },
+
+  scheduleButton: {
+    backgroundColor: "#3284f1",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 8,
+    justifyContent: "center",
+    width: "100%",
+    height: 50,
+    alignItems: "center",
+    marginBottom: 15,
+    marginTop: 15,
+  },
+
+  scheduleButtonText: {
+    color: "white",
+    fontSize: RFValue(18),
+    fontWeight: "bold",
+  },
+
+  goBack: {
+    borderWidth: 2,
+    borderColor: "white",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 8,
+    margin: "auto",
+    justifyContent: "center",
+    width: "100%",
+    height: 50,
+    alignItems: "center",
+    marginBottom: 15,
+    marginTop: 15,
+  },
+
+  goBackText: {
+    color: "#0D47AB",
+    fontSize: RFValue(18),
+    fontWeight: "bold",
+  },
+
+  // footer
+  footerView: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    height: 65,
+    borderRadius: 2,
+    borderTopLeftRadius: 18,
+    borderTopRightRadius: 18,
+    backgroundColor: "#3284f1",
+    elevation: 8,
+    borderTopWidth: 1,
+    borderColor: "white",
+  },
+
+  footerButton: {
+    flex: 1,
+    alignItems: "center",
+    color: "#FFF",
+  },
+
+  footerText: {
+    color: "#FFF",
+  },
+});
