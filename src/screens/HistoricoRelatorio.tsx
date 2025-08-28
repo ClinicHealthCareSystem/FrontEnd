2d62d85fc7da2b70e6e34f7ad10383587ce737f3
João Vithor <joaovithor@alu.ufc.br>
2025-08-28 14:45:50 -0300

    Configura tela para mostrar cabeçalho e corpo inicial (body)

--- src/screens/HistoricoRelatorio.tsx
+++ src/screens/HistoricoRelatorio.tsx
@@ -1,8 +1,17 @@
 import { SafeAreaView } from "react-native-safe-area-context";
-import { Text } from "react-native";
+import { ScrollView, Text, Image, View } from "react-native";
+import headerHome from "../stylesComponents/headerHome";
+import HeaderHome from "../components/headerHome";
 
 export default function HistoricoRelatorio(){
-    return(<SafeAreaView>
-        <Text>Hello World</Text>
-    </SafeAreaView>);
+    return(<ScrollView>
+        <HeaderHome ></HeaderHome>
+        <ScrollView>
+            <View>
+                <Text style={{marginTop:50}}>
+                    testando espaçamento
+                </Text>
+            </View>
+        </ScrollView>
+    </ScrollView>);
 }
