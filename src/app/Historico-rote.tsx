e9c814d2d9fac63224e9d0050c2ccfa4d2978415
João Vithor <joaovithor@alu.ufc.br>
2025-08-28 08:35:20 -0300

    Criação e configuração: Tela de Historico

---
+++ src/app/Historico-rote.tsx
@@ -0,0 +1,5 @@
+import HistoricoRelatorio from "../screens/HistoricoRelatorio";
+
+export default function Historico_rote(){
+    return(<HistoricoRelatorio/>);
+}
---
+++ src/screens/HistoricoRelatorio.tsx
@@ -0,0 +1,8 @@
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Text } from "react-native";
+
+export default function HistoricoRelatorio(){
+    return(<SafeAreaView>
+        <Text>Hello World</Text>
+    </SafeAreaView>);
+}
