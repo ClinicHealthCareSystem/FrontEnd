45b8d9a52b3e703560376884d3e464ee007dd7ae
Lucas <lucasvieiraribeiro2055@gmail.com>
2025-09-01 21:36:30 -0300

    TSk22  fix ajusta card de consulta

--- src/components/agendaCard.tsx
+++ src/components/agendaCard.tsx
@@ -5,6 +5,7 @@ import styles from "../stylesComponents/agendaCard";
 
 export default function AgendaCard() {
   return (
+    
     <View style={styles.CardContainer}>
       <Text style={styles.DateHour}>01/09/2025 (Segunda-Feira) 13:50</Text>
       <hr style={{ width: "90%" }} />
@@ -13,12 +14,21 @@ export default function AgendaCard() {
       <hr style={{ width: "90%" }} />
       <Text style={styles.Paciente}>Nome do paciente</Text>
       <Text style={styles.PacienteName}>Chaves</Text>
+
+      <View style={styles.caixaButtonOption}>
+        <View style={styles.buttonEdit}>
       <TouchableOpacity>
-        <Text style={styles.EditarAgendamentos}>Editar Agendamento</Text>
+        <Text style={styles.EditarAgendamentos}>Editar</Text>
       </TouchableOpacity>
+      </View>
+
+      <View style={styles.buttonCancel}>
       <TouchableOpacity>
-        <Text style={styles.Cancelar}>Cancelar Agendamento</Text>
+        <Text style={styles.Cancelar}>Cancelar</Text>
       </TouchableOpacity>
+      </View>
+      </View>
     </View>
+    
   );
 }
