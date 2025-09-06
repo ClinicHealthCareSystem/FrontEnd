e426b7f2e9764cd89483700a2d7ff27e2e7d0f5c
Davi Guerra <daviguerradesousa@gmail.com>
2025-09-06 14:34:11 -0300

    TSK18-Termina tela de planos

--- src/styles/planos.ts
+++ src/styles/planos.ts
@@ -22,33 +22,64 @@ subtitle: {
 },
 
 descricaocard: {
- 
+ fontSize:RFValue (16),
+  color: 'white',
+  marginTop: 10,
+  marginBottom: 10,
+  textAlign: 'left',
 },
 
 price: {
-  
+  fontSize: RFValue(18),
+  fontWeight: 'bold',
+  color: 'white',
+  marginBottom: 8,
+  textAlign: 'center',
 },
 
 card: {
   width: '92%',
   backgroundColor: '#0D47AB',
   borderRadius: 12,
+  padding: 18,
+  alignSelf: 'center',
+  justifyContent: 'space-between',
+  marginBottom: 25,
+  marginTop: 20,
+  shadowOffset: { width: 0, height: 2 },
+  shadowOpacity: 0.7,
+  shadowRadius: 8,
 },
 
 tagsContainer: {
-  
+  flexDirection: 'row',
+  flexWrap: 'wrap',
+  justifyContent: 'center',
+  marginVertical: 10,
 },
 
 tag: {
-  
+  color: 'white',
+  fontSize: RFValue(16),
+  fontWeight: 'bold',
+  paddingVertical: 4,
+  paddingHorizontal: 10,
 },
 
 buttoncard: {
-  
+  width: '100%',
+  backgroundColor: '#3284f1',
+  justifyContent: 'center',
+  alignItems: 'center',
+  borderRadius: 10,
+  paddingVertical: 10,
+  marginTop: 5,
 },
 
 buttoncardtext: {
-  
+  color: 'white',
+  fontSize: RFValue(18),
+  fontWeight: 'bold',
 },
 
  })
